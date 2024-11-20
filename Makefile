ea84393d6b86d60e5536594dc95180349f6ebe21
Tanner <tanner.wendland@gmail.com>
2024-11-20 06:31:51 -0500

    Adjusted uc client test code to use a testcontainer.

--- Makefile
+++ Makefile
@@ -1,9 +1,20 @@
 # Simple Makefile for a Go project
 
-UC_TAG := v0.2.0
+# Unity Catalog Submodule version
+UC_VERSION := 0.2.0
+UC_TAG := v$(UC_VERSION)
+
+# Determine if we need platform flag (for ARM-based systems)
+# Unity Catalog's docker container does not currently build for ARM systems.
+ARCH := $(shell uname -m)
+ifeq ($(ARCH),arm64)
+    PLATFORM_FLAG := --platform linux/x86_64/v8
+else
+    PLATFORM_FLAG :=
+endif
 
 # Build the application
-all: submodules generate build test
+all: submodules generate build uc-docker test
 
 submodules:
 	@git submodule update --init --recursive
@@ -35,26 +46,13 @@ generate:
 	@echo "Generating unity catalog client code..."
 	@go generate ./...
 
+# Create a unity catalog docker container. Required for
 uc-docker:
-	# TODO: Find a better way to tag this container
 	# TODO: Remove platform target when possible, UC image does not build on ARM.
-	@docker build -t unitycatalog/0.2.0 --platform linux/x86_64/v8 submodules/unity_catalog
+	@docker build -t unitycatalog/$(UC_VERSION) $(PLATFORM_FLAG) submodules/unity_catalog
 
-# Live Reload
-watch:
-	@if command -v air > /dev/null; then \
-			air; \
-			echo "Watching...";\
-		else \
-			read -p "Go's 'air' is not installed on your machine. Do you want to install it? [Y/n] " choice; \
-			if [ "$$choice" != "n" ] && [ "$$choice" != "N" ]; then \
-				go install github.com/air-verse/air@latest; \
-				air; \
-				echo "Watching...";\
-			else \
-				echo "You chose not to install air. Exiting..."; \
-				exit 1; \
-			fi; \
-		fi
+# Run Unity Catalog in a docker container that's ephemeral, only for testing.
+run-uc:
+	@docker run -p 8080:8080 unitycatalog/$(UC_VERSION)
 
-.PHONY: all submodules build run test clean generate watch
+.PHONY: all submodules build run test clean generate uc-docker run-uc
--- internal/uc_client/uc_client_test.go
+++ internal/uc_client/uc_client_test.go
@@ -2,11 +2,17 @@ package uc_client
 
 import (
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 	"testing"
+	"time"
 )
+
+// Image name for our unity catalog test container, built from this repo.
+// TODO: When UC docker is published to dockerhub use that instead
+const UC_IMAGE = "unitycatalog/0.2.0"
 
 type UcTestSuite struct {
 	suite.Suite
@@ -17,14 +23,14 @@ type UcTestSuite struct {
 func (s *UcTestSuite) SetupSuite() {
 	s.ctx = context.Background()
 	req := testcontainers.ContainerRequest{
-		// TODO: Use better image tag
-		Image:        "unitycatalog/0.2.0",
+		Image:        UC_IMAGE,
 		ExposedPorts: []string{"8080/tcp"},
-		WaitingFor:   wait.ForHealthCheck(),
+		WaitingFor:   wait.ForExposedPort().WithStartupTimeout(120 * time.Second),
 	}
 	container, err := testcontainers.GenericContainer(s.ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
 		Started:          true,
+		ProviderType:     testcontainers.ProviderPodman, // TODO: Programmatically set this
 	})
 	if err != nil {
 		s.T().Fatal(err)
@@ -32,29 +38,50 @@ func (s *UcTestSuite) SetupSuite() {
 	s.container = &container
 }
 
-// Individual test methods
-func (s *UcTestSuite) TestSomething() {
-	// Use s.repository for testing
+// Stop container after completion
+func (s *UcTestSuite) TearDownSuite() {
+	defer func() {
+		c := *s.container
+		err := c.Terminate(s.ctx)
+		if err != nil {
+			s.Errorf(err, "Failed to terminate container")
+		}
+	}()
 }
 
-func TestSuite(t *testing.T) {
-	suite.Run(t, new(UcTestSuite))
+// Utility function to create our generated UC Client.
+func GetUcClient(s *UcTestSuite) *ClientWithResponses {
+	c := *s.container
+	port, err := c.MappedPort(context.Background(), "8080")
+	if err != nil {
+		panic(err)
+	}
+	host := fmt.Sprintf("http://localhost:%s/api/2.1/unity-catalog", port.Port())
+	client, err := NewClientWithResponses(host)
+	if err != nil {
+		s.Errorf(err, "Failed to create client")
+	}
+	return client
 }
 
 // Testing the initialization of the UC OSS generated client
-func TestInitClient(t *testing.T) {
-	ctx := context.Background()
-	c, err := NewClientWithResponses("http://localhost:8080/api/2.1/unity-catalog")
-	if err != nil {
-		t.Errorf("Failed to create client: %v", err)
-	}
+// For now we're going to trust that the client itself is generated correctly
+// From the OpenAPI spec, as long as our client code is generated.
+func (s *UcTestSuite) TestInitialClient() {
+	c := GetUcClient(s)
 	p := ListCatalogsParams{}
-	catalogs, err := c.ListCatalogsWithResponse(ctx, &p)
+	catalogs, err := c.ListCatalogsWithResponse(s.ctx, &p)
 	if err != nil {
-		t.Errorf("Failed to list catalogs: %v", err)
+		s.Errorf(err, "Failed to list catalogs: %v", err)
 	}
 	// UC default deployment starts with a "unity" catalog initially
 	if len(*catalogs.JSON200.Catalogs) != 1 {
-		t.Errorf("Expected catalogs size to be zero")
+		s.Errorf(err, "Expected catalogs size to be one! (the default)")
 	}
+}
+
+// Custom Test Suite to use unity catalog container.cccccbktbvetvcnirvdgltknbjbglvrneueudtjubvdc
+func TestSuite(t *testing.T) {
+	s := new(UcTestSuite)
+	suite.Run(t, s)
 }
